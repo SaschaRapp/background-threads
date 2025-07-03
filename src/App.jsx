6d716a8b644ae9b3c799a7b0ad0c028ba82bbd2e
SaschaRapp <50624095+SaschaRapp@users.noreply.github.com>
2025-07-03 18:03:55 +0200

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Threads from "./Threads"; // Make sure the path matches
+
+function App() {
+  return (
+    <div className="App">
+      <Threads />
+    </div>
+  );
+}
+
+export default App;
